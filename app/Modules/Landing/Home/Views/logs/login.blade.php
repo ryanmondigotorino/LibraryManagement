b2dfa24df1ea77140018d1ba91e53b1e02ae97aa
Henrieta W. Avelino <henrietaavelino.a99ojt@gmail.com>
2019-04-20 01:13:05 +0800

    Login Page and Sign Up Page (In Progress)

--- app/Modules/Landing/Home/Views/logs/login.blade.php
+++ app/Modules/Landing/Home/Views/logs/login.blade.php
@@ -5,24 +5,24 @@
 @section('content')
 <div class="login_container">
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-6 col-xl-6">
             <div id="slider" class="carousel slide" data-ride="carousel">
 
                 <ul class="carousel-indicators">
-                    <li data-target="#slider" data-slide-to="0" class="active"></li>
-                    <li data-target="#slider" data-slide-to="1"></li>
+                    <li data-target="#slider" data-slide-to="0"></li>
+                    <li data-target="#slider" data-slide-to="1"  class="active"></li>
                     <li data-target="#slider" data-slide-to="2"></li>
                 </ul>
 
                 <div class="carousel-inner">
+                    <div class="carousel-item">
+                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/books.jpg')}}" alt="Floating Books">
+                    </div>
                     <div class="carousel-item active">
-                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/books.jpg')}}" alt="Los Angeles">
+                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/shelf.jpg')}}" alt="Books">
                     </div>
                     <div class="carousel-item">
-                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/shelf.jpg')}}" alt="Chicago">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/library.jpg')}}" alt="New York">
+                    <img class="img-fluid" src="{{URL::asset('storage/uploads/login_images/library.jpg')}}" alt="Library">
                     </div>
                 </div>
 
@@ -35,10 +35,11 @@
 
             </div>
         </div>
-        <div class="col-lg-6">
-            <h2 class="text-center">Welcome Back!</h2><hr>
+        <div class="col-lg-6 col-xl-6 mt-5 login_form">
+            <h2 class="text-center bold">Welcome Back!</h2>
             <div class="row" style="margin-bottom:2%;">
-                <div class="col-lg-12">
+                <div class="col-lg-7 col-xl-8 text-center ml-auto mr-auto">
+                    <hr>
                     <form class="loginClick">
                         {{ csrf_field() }} 
                         <div class="form-group">
@@ -53,7 +54,7 @@
                             <button type="submit" class="col-md-12 btn btn-default login_button" style="background-color:#800; color:#fff;" name="sbmt">Login</button>
                         </div>
                     </form>
-                    <small>Don't have an account? <a href="" class="create_link"> REGISTER </a></small>
+                    <small>Don't have an account? <a href="{{route('landing.home.sign-up')}}" class="create_link"> REGISTER. </a></small>
                 </div>
             </div>
         </div>
