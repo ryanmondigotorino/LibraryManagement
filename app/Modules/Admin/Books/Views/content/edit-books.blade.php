95c0e6b174480437cb1bdbfa0bfb360b1d6f8a1e
Ryan <papathorstorino@gmail.com>
2019-04-27 02:12:25 +0800

    Add Edit Books In-progress view books

--- app/Modules/Admin/Books/Views/content/edit-books.blade.php
+++ app/Modules/Admin/Books/Views/content/edit-books.blade.php
@@ -10,18 +10,18 @@
             <div class="card" style="width:100%">
                 <div class="card-body">
                     <div class="profile_content">
-                        <h1 class="h2"><span class="fa fa-plus"></span> Add books</h1><hr>
+                        <h1 class="h2"><span class="fa fa-edit"></span> Edit books</h1><hr>
                         <div class="row">
                             <div class="col-lg-2"></div>
                             <div class="col-lg-8">
-                                <form action="{{route('admin.books.add-books-save')}}" class="add-books">
+                                <form action="{{route('admin.books.edit-books-save')}}" class="edit-books">
                                     {{ csrf_field() }}
                                     <div class="row">
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label for="frontimage">Front Book Image</label>
                                                 <div class="form-group img-group">
-                                                    <img src="{{URL::asset('storage/uploads/profile_image/noimage.png')}}" id="book-front-picture" class="item_image btn_book-front-picture" alt="Book Front Image">
+                                                    <img src="{{URL::asset('storage/uploads/book_images/book-('.$getBooks[0]->id.')/'.$getBooks[0]->front_image)}}" id="book-front-picture" class="item_image btn_book-front-picture" alt="Book Front Image">
                                                     <input type="file" class="d-none" name="frontImage"/>
                                                 </div>
                                             </div>
@@ -30,7 +30,7 @@
                                             <label for="frontimage">Back Book Image</label>
                                             <div class="form-group">
                                                 <div class="form-group img-group">
-                                                    <img src="{{URL::asset('storage/uploads/profile_image/noimage.png')}}" id="book-back-picture" class="item_image btn_book-back-picture" alt="Book Back Image">
+                                                    <img src="{{URL::asset('storage/uploads/book_images/book-('.$getBooks[0]->id.')/'.$getBooks[0]->back_image)}}" id="book-back-picture" class="item_image btn_book-back-picture" alt="Book Back Image">
                                                     <input type="file" class="d-none" name="backImage"/>
                                                 </div>
                                             </div>
@@ -39,35 +39,36 @@
                                     <div class="form-group">
                                         <label for="booktitle">Book Author</label>
                                         <select name="book_author" class="form-control">
-                                            <option selected disabled>Choose...</option>
+                                            <option disabled>Choose...</option>
                                             @foreach($getAuthors as $authors)
-                                                <option value="{{$authors->id}}">{{$authors->name}}</option>
+                                                <option {{$getBooks[0]->author_id == $authors->id ? 'selected' : ''}} value="{{$authors->id}}">{{$authors->name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                     <div class="form-group">
                                         <label for="booktitle">Book Title</label>
-                                        <input type="text" class="form-control" name="book_title" placeholder="Enter Book Title">
+                                        <input type="text" class="form-control" name="book_title" value="{{$getBooks[0]->title}}" placeholder="Enter Book Title">
                                     </div>
                                     <div class="form-group">
                                         <label for="booktitle">Book Genre</label>
                                         <select name="book_genre" class="form-control">
-                                            <option selected disabled>Choose...</option>
+                                            <option disabled>Choose...</option>
                                             @foreach(config('books.book_category') as $genre)
-                                                <option value="{{$genre}}">{{$genre}}</option>
+                                                <option {{$getBooks[0]->genre == $genre ? 'selected' : ''}} value="{{$genre}}">{{$genre}}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                     <div class="form-group">
                                         <label for="booktitle">Book Description</label>
-                                        <textarea name="book_description" rows="5" class="form-control" placeholder="Enter Book Description (Optional)"></textarea>
+                                        <textarea name="book_description" rows="5" class="form-control" placeholder="Enter Book Description (Optional)">{{$getBooks[0]->description}}</textarea>
                                     </div>
                                     <div class="form-group">
                                         <label for="booktitle">Date Published</label>
-                                        <input type="date" class="form-control" max="{{date('Y-m-d',time())}}" name="book_published">
+                                        <input type="date" class="form-control" max="{{date('Y-m-d',time())}}" value="{{date('Y-m-d',$getBooks[0]->date_published)}}" name="book_published">
                                     </div>
+                                    <input type="hidden" name="book_id" value="{{$getBooks[0]->id}}">
                                     <div class="form-group">
-                                        <button class="btn btn-secondary add-books pull-right" type="submit"><span class="fa fa-plus"></span> Add</button>
+                                        <button class="btn btn-secondary edit-books pull-right" type="submit"><span class="fa fa-edit"></span> Edit</button>
                                     </div>
                                 </form>
                             </div>
@@ -90,7 +91,7 @@
             $('.item_image').css('opacity','1');
             var reader = new FileReader();
             reader.onload = function(e){
-                $('#book-front-picture').attr('src',e.target.result);
+                $('img.btn_book-front-picture#book-front-picture').attr('src',e.target.result);
             }
             if($('input[type="file"][name="frontImage"]')[0]['files'][0]){
                 reader.readAsDataURL($("input[type='file'][name='frontImage']")[0]['files'][0]);
@@ -104,7 +105,7 @@
             $('.item_image').css('opacity','1');
             var reader = new FileReader();
             reader.onload = function(e){
-                $('#book-back-picture').attr('src',e.target.result);
+                $('img.btn_book-back-picture#book-back-picture').attr('src',e.target.result);
             }
             if($('input[type="file"][name="backImage"]')[0]['files'][0]){
                 reader.readAsDataURL($("input[type='file'][name='backImage']")[0]['files'][0]);
@@ -112,7 +113,7 @@
             }
         });
     });
-    $('form.add-books').on('submit',function(event){
+    $('form.edit-books').on('submit',function(event){
         event.preventDefault();
         var frontImage = globalFrontImage;
         var backImage = globalBackImage;
@@ -120,6 +121,7 @@
         formData.append('book_front',frontImage);
         formData.append('book_back',backImage);
         formData.append('book_author',$('select[name="book_author"]').val());
+        formData.append('book_id',$('input[name="book_id"]').val());
         formData.append('book_title',$('input[name="book_title"]').val());
         formData.append('book_genre',$('select[name="book_genre"]').val());
         formData.append('book_description',$('textarea[name="book_description"]').val());
@@ -132,8 +134,8 @@
             contentType: false,
             processData: false,
             beforeSend:function(){
-                $('button[type="submit"].add-books').prop('disabled',true);
-                $('button[type="submit"].add-books').html('<i class="fa fa-spinner fa-pulse"></i>');
+                $('button[type="submit"].edit-books').prop('disabled',true);
+                $('button[type="submit"].edit-books').html('<i class="fa fa-spinner fa-pulse"></i>');
             },
             success: function(result){
                 if(result['status'] == 'success'){
@@ -145,7 +147,7 @@
                         location.href="{{route('admin.books.index')}}";
                     });
                 }else{
-                    $('button[type="submit"].add-books').prop('disabled',false);
+                    $('button[type="submit"].edit-books').prop('disabled',false);
                     swal({
                         title: 'Error!',
                         text: result['messages'],
@@ -154,7 +156,7 @@
                 }
             }
         }).done(function(){
-            $('button[type="submit"].add-books').html('<span class="fa fa-plus"></span> Add');
+            $('button[type="submit"].edit-books').html('<span class="fa fa-edit"></span> Edit');
         });
     });
 </script>
